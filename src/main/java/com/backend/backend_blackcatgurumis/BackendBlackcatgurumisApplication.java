0120c389f7018e064f556983ad7ffd6670dec02f
darley523 <gac.av@hotmail.com>
2025-10-29 00:43:09 -0300

    primer commit - config

---
+++ src/main/java/com/backend/backend_blackcatgurumis/BackendBlackcatgurumisApplication.java
@@ -0,0 +1,13 @@
+package com.backend.backend_blackcatgurumis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBlackcatgurumisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBlackcatgurumisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/backend_blackcatgurumis/BackendBlackcatgurumisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.backend_blackcatgurumis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBlackcatgurumisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
