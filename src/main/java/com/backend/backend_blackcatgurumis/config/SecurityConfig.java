ba269e97785fd1705108d33a19b3cf89ea43c2ec
darley523 <gac.av@hotmail.com>
2025-10-30 16:08:48 -0300

    comentarios

--- src/main/java/com/backend/backend_blackcatgurumis/config/SecurityConfig.java
+++ src/main/java/com/backend/backend_blackcatgurumis/config/SecurityConfig.java
@@ -12,10 +12,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration; // IMPORTAR
-import org.springframework.web.cors.CorsConfigurationSource; // IMPORTAR
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource; // IMPORTAR
-import java.util.Arrays; // IMPORTAR
+import org.springframework.web.cors.CorsConfiguration; // 
+import org.springframework.web.cors.CorsConfigurationSource; // 
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource; //
+import java.util.Arrays;
 
 import com.backend.backend_blackcatgurumis.config.filters.JwtAuthenticationFilter;
 import com.backend.backend_blackcatgurumis.services.UserDetailsServiceImpl;
